3b96f1162fdf56c7db94f3f2c87f0b54ac24cbeb
Ducratif <ducratiff@proton.me>
2025-11-05 17:37:56 +0100

    upload

---
+++ resources/[2_test]/[1_framework]/screenshot-basic/__resource.lua
@@ -0,0 +1,36 @@
+﻿--[[
+  GuerreRP - Base publique
+  Développé et maintenu par Ducratif
+
+  Discord : https://discord.gg/kpD8pQBBWm
+  Documentation : https://github.com/Ducratif/guerrerp
+
+  Merci de ne pas retirer ce crédit si vous utilisez ou modifiez cette ressource.
+]]
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script 'dist/client.js'
+server_script 'dist/server.js'
+
+dependency 'yarn'
+dependency 'webpack'
+
+webpack_config 'client.config.js'
+webpack_config 'server.config.js'
+webpack_config 'ui.config.js'
+
+files {
+    'dist/ui.html'
+}
+
+ui_page 'dist/ui.html'
+
+--[[
+  GuerreRP - Base publique
+  Développé et maintenu par Ducratif
+
+  Discord : https://discord.gg/kpD8pQBBWm
+  Documentation : https://github.com/Ducratif/guerrerp
+
+  Merci de ne pas retirer ce crédit si vous utilisez ou modifiez cette ressource.
+]]
